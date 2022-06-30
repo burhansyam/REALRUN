ef00fa4bc505c801845ce6b26e7d8c20613ef399
MAOU BOTZ <95448883+VERDIBOTZ1@users.noreply.github.com>
2022-07-01 06:15:15 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,21 +1,21 @@
 //=============『 Utama 』================== //
-global.owner = ['6282221792667'] 
+global.owner = ['6285706735450'] 
 global.mods = [] 
 global.prems = [] 
 
 //=============『 Info Owner 』============== //
-global.nameowner = 'OwnerTio [Jangan chat owner kl gk perlu]'
-global.numberowner = '6282221792667'
-global.instagram = 'https://instagram.com/prm2.0'
-global.github = 'https://github.com/BOTCAHX'
-global.dana = '082221792667'
-global.pulsa = '082221792667'
+global.nameowner = 'verdi [Jangan chat owner kalo cwek gpp🐦]'
+global.numberowner = '6285706735450'
+global.instagram = 'https://instagram.com/'
+global.github = 'https://github.com/VERDIBOTZ1'
+global.dana = '0'
+global.pulsa = '0'
 global.gopay = 'Coming soon'
 
 //=============『 Info Bot 』=================//
-global.namebot = 'BOTCAHX'
+global.namebot = 'XziroID'
 global.gc = 'https://chat.whatsapp.com/JEDQfzMGZan4HA98wtxsNL'
-global.web = 'https://myblogxyz.netlify.app' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
+global.web = 'https://games.vercel.app' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
 global.price1 = '1 bulan = 10.000\n2 Minggu 7.000\n1 Minggu 5.000'
 
 //=======『 Tampilan Dan Lainnya 』============//
@@ -23,10 +23,10 @@ global.fotonya1 = 'https://telegra.ph/file/e4a2f4339da8a32ad20a1.jpg' //ganti ja
 global.fotonya2 = 'https://telegra.ph/file/0a08fbd322e2347d4c45b.jpg' //ini juga ganti 
 global.lolkey = 'Papah-Chan' //biar mudah ngegantinya semisal apikeynya expired:v
 global.zenzkey = 'BagasPrdn' //ganti jadi apikey lu kalau expired
-global.wm = '© TioXd'
+global.wm = '© VERDI.xzy'
 global.watermark = wm
-global.wm2 = '            「 © Made with ♥ by TioXd⁩ 」'
-global.wm3 = '© TioXd'
+global.wm2 = '            「 © Made with ♥ by VERDI.XCP⁩ 」'
+global.wm3 = '© VERDI.xzy'
 global.htki = '––––––『' 
 global.htka = '』––––––'
 global.media = 'https://telegra.ph/file/72168c6f1aaa90315c369.jpg'
@@ -37,7 +37,7 @@ global.benar = 'Benar ✅\n'
 global.salah = 'Salah ❌\n'
 global.stiker_wait = '⫹⫺ Stiker sedang dibuat...'
 global.packname = '⫹⫺ Made with'
-global.author = '© Bot Sticker\nBy TioXd\n\nig:prm2.0\n\n© BOTCAHX'
+global.author = '© Bot Sticker\nBy VERDI\n\nig:.0\n\n© XZIROID'
 
 //=============『 Apikey 』================== //
 global.APIs = { // API Prefix
